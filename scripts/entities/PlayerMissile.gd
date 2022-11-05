2a13b8acff33251346910ee8da8d10e05af3eaaa
Marcus Sanatan <msanatan@gmail.com>
2022-11-05 16:49:20 -0400

    Follow the mouse position after being fired
    Not that hard to implement, works well

--- scripts/entities/PlayerMissile.gd
+++ scripts/entities/PlayerMissile.gd
@@ -2,11 +2,24 @@ extends Area2D
 
 export (int) var speed = 200
 var velocity = Vector2(0, 0)
+var angle = 0
+var is_following_mouse = false
 
 func _ready():
     velocity = Vector2.RIGHT
 
+
 func _process(delta):
-    velocity = Vector2.RIGHT.rotated(rotation) * speed * delta
+    var mouse_position = get_global_mouse_position()
+    if not is_following_mouse:
+        angle = rotation
+    else:
+        angle = (mouse_position - global_position).angle()
+
+    look_at(mouse_position)
+    velocity = Vector2.RIGHT.rotated(angle) * speed * delta
     translate(velocity)
 
+
+func _on_FollowCursorTimer_timeout():
+    is_following_mouse = true
