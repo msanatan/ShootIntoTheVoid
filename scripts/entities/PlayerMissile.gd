6a9533b925348eed2394da4b4804fd91b12127ca
Marcus Sanatan <msanatan@gmail.com>
2022-11-06 21:44:57 -0400

    17 fixes to player and shooting (#20)
    * Add a Node2D spawn point for bullets
    
    It's a bit further away, and as a child node has the same rotation
    
    * Use *nix line endings in Git
    
    Kept getting file changes as I switched from Mac to Windows.
    This seems to keep it consistent
    
    * When the player doesn't move the cursor, their missile continues in the same direction
    
    It's weird when it's stuck by the mouse if it catches it.
    It works, but we probably have to add a lerp function to make changes in direction smoother.
    Same could be said for the player facing the angle.
    
    * Destroy missile when it's out of the view
    
    * Only run pipelines when branch is merged into dev
    
    My bad, just noticed that mistake

--- scripts/entities/PlayerMissile.gd
+++ scripts/entities/PlayerMissile.gd
@@ -6,21 +6,32 @@ export (int) var speed = 200
 var velocity = Vector2(0, 0)
 var angle = 0
 var is_following_mouse = false
+var recalculate_angle = false
+var previous_cursor_position = Vector2.ZERO
 
 func _ready():
 	velocity = Vector2.RIGHT
 
 
 func _process(delta):
-	var mouse_position = get_global_mouse_position()
-	if not is_following_mouse:
-		angle = rotation
+	var cursor_position = get_global_mouse_position()
+	if previous_cursor_position == cursor_position:
+		recalculate_angle = false
 	else:
-		angle = (mouse_position - global_position).angle()
+		previous_cursor_position = cursor_position
+		recalculate_angle = true
 
-	look_at(mouse_position)
-	velocity = Vector2.RIGHT.rotated(angle) * speed * delta
-	translate(velocity)
+	if recalculate_angle:
+		if not is_following_mouse:
+			angle = rotation
+		else:
+			angle = (cursor_position - global_position).angle()
+
+		look_at(cursor_position)
+		velocity = Vector2.RIGHT.rotated(angle) * speed * delta
+		translate(velocity)
+	else:
+		translate(velocity)
 
 
 func _on_FollowCursorTimer_timeout():
@@ -28,6 +39,12 @@ func _on_FollowCursorTimer_timeout():
 
 
 func _on_DestroyTimer_timeout():
-	print_debug("Missile destroyed")
+	print_debug("Missile time ran out")
+	emit_signal("missile_destroyed")
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	print_debug("Missile out of bounds")
 	emit_signal("missile_destroyed")
 	queue_free()
