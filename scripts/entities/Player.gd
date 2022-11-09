053d89c28a1a822757df33d13b7ee6a7fb164be0
Marcus Sanatan <msanatan@gmail.com>
2022-11-08 22:53:59 -0400

    Allow for player movement before shooting
    Now the player can hide behind obstacles, to prevent shots.
    Or increase their chance of hitting more targets!

--- scripts/entities/Player.gd
+++ scripts/entities/Player.gd
@@ -2,16 +2,16 @@ extends KinematicBody2D
 
 signal fire_missile
 signal missile_destroyed
-
-export (PackedScene) var missile
-var is_shooting = false
-
-export var health = 100
-var score = 0
-
 signal health_decreased
 signal player_died
 signal score_increased
+
+export (PackedScene) var missile
+export var health = 100
+export var speed = 150
+var is_shooting = false
+var score = 0
+var velocity = Vector2.ZERO
 
 func _input(event):
 	if event.is_action_pressed("attack") and not is_shooting:
@@ -23,7 +23,19 @@ func _input(event):
 
 func _physics_process(delta):
 	if not is_shooting:
+		velocity = Vector2.ZERO
 		look_at(get_global_mouse_position())
+
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		elif Input.is_action_pressed("move_right"):
+			velocity.x += 1
+		elif Input.is_action_pressed("move_up"):
+			velocity.y -= 1
+		elif Input.is_action_pressed("move_down"):
+			velocity.y += 1
+
+		move_and_collide(velocity.normalized() * speed * delta)
 
 
 func _on_PlayerMissile_missile_destroyed():
@@ -40,19 +52,19 @@ func _on_AnimationPlayer_animation_finished(anim_name:String):
 		spawned_missile.look_at(get_global_mouse_position())
 		spawned_missile.connect("missile_destroyed", self, "_on_PlayerMissile_missile_destroyed")
 		spawned_missile.connect("enemy_hit", self, "_on_PlayerMissile_enemy_hit")
-	
+
 func _on_PlayerMissile_enemy_hit(enemy):
 	enemy.kill()
 	increaseScore(100)
-	
+
 func increaseScore(amount):
 	score += amount
 	emit_signal("score_increased", score)
-	
+
 func decreaseHealth(amount):
 	health -= amount
 	emit_signal("health_decreased", health)
-	
+
 	if health < 0:
 		health = 0
 		emit_signal("player_died")
