00d436a6d623645681aed8911b536f438d85beea
Hsaka <akash.harriram@gmail.com>
2022-11-11 22:22:19 -0400

    Add level transitions

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,11 +1,12 @@
 extends Node
 
 var game_over = false
+var rand = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rand.randomize()
 	$EnemyManager.spawn_objects_for_level(Globals.level)
-
 	$UI/HealthLabel.set_text("LIFE: "+str(Globals.health))
 	$UI/ScoreLabel.set_text("SCORE: "+str(Globals.score))
 	$UI/LevelLabel.set_text("LEVEL: "+str(Globals.level))
@@ -32,13 +33,47 @@ func _process(_delta):
 
 
 func _on_level_cleared():
-	#game_over = true
-	#$UI/LevelCompleteLabel.show()
-	#$UI/DemoLabel.show()
-	#$UI/RestartButton.show()
-	#$UI/TurnLabel.hide()
 	Globals.level += 1
+	do_random_transition()
+	queue_free()
 	get_tree().reload_current_scene()
+	
+	
+func do_random_transition():
+	var rand_val = rand.randi_range(0, 15)
+	match rand_val:
+		0: 
+			FancyFade.noise(self)
+		1: 
+			FancyFade.pixelated_noise(self)
+		2:
+			FancyFade.blurry_noise(self)
+		3:
+			FancyFade.cell_noise(self)
+		4:
+			FancyFade.wipe_left(self)
+		5:
+			FancyFade.wipe_right(self)
+		6:
+			FancyFade.wipe_up(self)
+		7:
+			FancyFade.wipe_down(self)
+		8:
+			FancyFade.wipe_square(self)
+		9:
+			FancyFade.wipe_conical(self)
+		10:
+			FancyFade.circle_in(self)
+		11:
+			FancyFade.circle_out(self)
+		12:
+			FancyFade.horizontal_paint_brush(self)
+		13:
+			FancyFade.vertical_paint_brush(self)
+		14:
+			FancyFade.swirl(self)
+		15:
+			FancyFade.tile_reveal(self)
 
 func _on_game_over():
 	game_over = true
@@ -60,7 +95,8 @@ func _on_Player_turn_ended():
 		show_turn_label("Enemy Turn")
 	
 func _on_Player_turn_started():
-	show_turn_label("Player Turn")
+	if !game_over:
+		show_turn_label("Player Turn")
 	
 func show_turn_label(text):
 	$UI/TurnLabel.show()
@@ -83,4 +119,7 @@ func _on_Player_missile_destroyed():
 
 
 func _on_RestartButton_pressed():
+	Globals.level = 1
+	Globals.health = 100
+	Globals.score = 0
 	get_tree().reload_current_scene()
