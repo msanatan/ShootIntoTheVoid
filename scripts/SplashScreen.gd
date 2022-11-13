4dffaccad235c8197fe3e818764ba3675e878bac
Marcus Sanatan <msanatan@gmail.com>
2022-11-13 00:08:19 -0400

    Create a custom splash screen with out logos.
    Got the idea from this doc:
    https://docs.godotengine.org/en/3.3/getting_started/step_by_step/splash_screen.html
    
    Just noticed how old it was and there may be a better way without an entire scene...
    
    However, for WebGL games, it looks like it's either this or custom webpage:
    https://docs.godotengine.org/en/stable/tutorials/export/exporting_for_web.html#boot-splash-is-not-displayed

--- scripts/SplashScreen.gd
+++ scripts/SplashScreen.gd
@@ -1,16 +1,7 @@
 extends MarginContainer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var fade_scene := $FadeScene
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _on_Timer_timeout():
+    fade_scene.transition_to()
