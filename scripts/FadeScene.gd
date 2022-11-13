3f0ea091bb59b6d4b8f73350b306a546b0b8adc2
Marcus Sanatan <msanatan@gmail.com>
2022-11-13 00:04:12 -0400

    Add a fade scene which FancyFade surprisingly didn't have
    Simple fade to black then out of it.

---
+++ scripts/FadeScene.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+export (String, FILE, "*.tscn") var next_scene
+
+onready var anim_player := $AnimationPlayer
+
+
+func _ready() -> void:
+	anim_player.play_backwards("Fade")
+
+
+func transition_to(_next_scene := next_scene) -> void:
+	anim_player.play("Fade")
+	yield(anim_player, "animation_finished")
+	get_tree().change_scene(_next_scene)
