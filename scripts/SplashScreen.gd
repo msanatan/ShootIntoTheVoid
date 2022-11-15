b9fdf60e4a0a70b0153fc102a957bceeb21a7455
Hsaka <akash.harriram@gmail.com>
2022-11-14 21:47:10 -0400

    Draw circular orbits for the obstacles

--- scripts/SplashScreen.gd
+++ scripts/SplashScreen.gd
@@ -4,4 +4,4 @@ onready var fade_scene := $FadeScene
 
 
 func _on_Timer_timeout():
-    fade_scene.transition_to()
+	fade_scene.transition_to()
--- scripts/entities/Obstacle.gd
+++ scripts/entities/Obstacle.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+export (PackedScene) var orbit_circle
+
 var count = 0
 var radius = 200
 var speed = 0.6
@@ -9,14 +11,20 @@ var rand = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rand.randomize()
+	rand.randomize()	
 	screen_center = get_viewport_rect().size / 2
 	init_motion()
 
 func init_motion():
 	radius = position.distance_to(screen_center)
 	speed = rand.randf_range(0.1, 0.6)
-	print(speed)
+	
+	var circle = orbit_circle.instance()
+	circle.size.x = radius*2
+	circle.size.y = radius*2
+	circle.position = screen_center
+	get_tree().get_root().get_node("Main/Orbits").add_child(circle)
+		
 	is_moving = true	
 
 func _process(delta):
