a862cfd60b73910679f0e52cd8df3ceb0b07df5d
Marcus Sanatan <msanatan@gmail.com>
2022-12-01 03:31:57 -0400

    Ensure I call randomize before calling the random function

--- scripts/entities/Enemy.gd
+++ scripts/entities/Enemy.gd
@@ -12,6 +12,7 @@ var order_num = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	is_shielded = $Shield.visible
 	is_ordered = $OrderLabel.visible
 
